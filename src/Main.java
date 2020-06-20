b4c644007a55fedea2b7504d37140fe0f182e6dc
miguemesen <miguemesen12@gmail.com>
2020-06-20 17:44:29 -0600

    Dummy commit

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,22 @@ public class Main {
     public static void main(String[] args){
         System.out.println("smart commit");
     }
+
+    public void xd(int number){
+        if (number==1){
+            System.out.println("xd");
+        }
+        if (number==2){
+            System.out.println("xd");
+        }
+        if (number==3){
+            System.out.println("xd");
+        }
+        if (number==4){
+            System.out.println("xd");
+        }
+        if (number==5){
+            System.out.println("xd");
+        }
+    }
 }
