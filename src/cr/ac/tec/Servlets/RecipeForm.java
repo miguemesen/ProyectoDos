2e9c917fe9ab693df352e98f0109d3e348647982
lazhero <54720338+lazhero@users.noreply.github.com>
2020-07-24 10:29:56 -0600

    [COOK-37] Fixing the servlet route

--- src/cr/ac/tec/Servlets/RecipeForm.java
+++ src/cr/ac/tec/Servlets/RecipeForm.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "RecipeForm",value = "/register")
+@WebServlet(name = "RecipeForm",value = "/RecipeGetter")
 public class RecipeForm extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
